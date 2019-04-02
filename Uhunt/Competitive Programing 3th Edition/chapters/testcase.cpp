7f905557211cf07456b0c1a6ae8868b8f801bcc4
hvzzzz <42120805+hvzzzz@users.noreply.github.com>
2019-04-02 01:10:37 -0500

    test

--- Uhunt/Competitive Programing 3th Edition/chapters/testcase.cpp
+++ Uhunt/Competitive Programing 3th Edition/chapters/testcase.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
 #include<cstdio>
+#define _USE_MATH_DEFINES
 #include<cmath>
+#include <iomanip> 
 using namespace std;
-int main()
+int main()//print pi with a given number of decimal places
 {
-
+    int len;
+    cin>>len;
+    cout<<setprecision(len+1)<<M_PI<<endl;
 }
