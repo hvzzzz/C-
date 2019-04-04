074321c66d33bbc5bc1f201ff494fe7e7839fda0
hvzzzz <42120805+hvzzzz@users.noreply.github.com>
2019-04-03 20:09:52 -0500

    playing with dates

--- Uhunt/Competitive Programing 3th Edition/chapters/dates.cpp
+++ Uhunt/Competitive Programing 3th Edition/chapters/dates.cpp
@@ -1,7 +1,10 @@
-#include<iostream>
-#include<ctime>
-using namespace std;
-int main()
+#include <stdio.h>      /* printf */
+#include <time.h>       /* time_t, time, ctime */
+int main ()
 {
-    
+    time_t rawtime;
+    struct tm * timeinfo;
+    int year, month ,day;
+    const char * weekday[] = { "Sunday", "Monday","Tuesday", "Wednesday","Thursday", "Friday", "Saturday"};
 }
+    
