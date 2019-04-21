8ea5d24b4e487b1b6db89bed8dae6a30d4cd7647
hvzzzz <42120805+hvzzzz@users.noreply.github.com>
2019-04-20 19:10:24 -0500

    solving

--- Uhunt/Competitive Programing 3th Edition/chapters/dates.cpp
+++ Uhunt/Competitive Programing 3th Edition/chapters/dates.cpp
@@ -1,23 +1,27 @@
 #include <stdio.h>      /* printf, scanf */
+#include<iostream>
 #include <time.h>       /* time_t, struct tm, time, mktime*/ 
-#include <cstring>
+#include <string>
+using namespace std;
 int main ()
 {
-    time_t rawtime;
-    struct tm * timeinfo;
-    int year, month ,day;
+    time_t fechalocal;
+    struct tm * info_time;
+    int year=2019, month=4 ,day;
     const char * weekday[] = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
   /* prompt user for date */
-    printf ("Enter year: "); scanf ("%d",&year);
-    printf ("Enter month: "); scanf ("%d",&month);
+    //printf ("Enter year: "); scanf ("%d",&year);
+    //printf ("Enter month: "); scanf ("%d",&month);
     printf ("Enter day: "); scanf ("%d",&day);
   /* get current timeinfo and modify it to the user's choice */
-    time ( &rawtime );
-    timeinfo = localtime ( &rawtime );
-    timeinfo->tm_year = year - 1900;
-    timeinfo->tm_mon = month - 1;
-    timeinfo->tm_mday = day;
+    time ( &fechalocal );
+    info_time = localtime ( &fechalocal );
+    info_time->tm_year = year - 1900;
+    info_time->tm_mon = month - 1;
+    info_time->tm_mday = day;
   /* call mktime: timeinfo->tm_wday will be set */
-    mktime ( timeinfo );
-    printf ("That day is a %s.\n", weekday[timeinfo->tm_wday]);
+    mktime ( info_time );
+    cout<<weekday[(*info_time).tm_wday]<<endl;
+    //printf ("That day is a %s.\n", weekday[info_time->tm_wday]);
 }
+//this was a little boring but I did it anyway
--- Uhunt/Competitive Programing 3th Edition/chapters/prove.cpp
+++ Uhunt/Competitive Programing 3th Edition/chapters/prove.cpp
@@ -1,15 +1,10 @@
-#include <iostream>
-#include<cstdio>
-using namespace std;
-int main () 
-{
-    int  var;
-    int  *ptr;//es el puntero
-    int  val;
-    var = 3000;
-    ptr = &var;//saca la direccion de ptr
-    val = *ptr;//saca el valor en ptr
-    cout << "Value of var :" << var << endl;
-    cout << "Value of ptr :" << ptr << endl;
-    cout << "Value of val :" << val << endl;
+#include <stdio.h>
+#include <time.h>
+
+int main () {
+   time_t seconds;
+
+   seconds = time(0);
+   printf("time = %ld\n", seconds);
+  
 }
